464e1bc3c50b44d7f37024a21f63e8c4b18e87c9
Shelby Sowell <39719925+ssowellsvt@users.noreply.github.com>
2019-07-18 19:51:58 -0500

    CoinRequest.io Integration
    Effortless invoicing via URL and social media

--- src/qt/receiverequestdialog.cpp
+++ src/qt/receiverequestdialog.cpp
@@ -11,14 +11,13 @@
 #include <qt/optionsmodel.h>
 
 #include <QClipboard>
+#include <QDesktopServices>
 #include <QDrag>
 #include <QMenu>
 #include <QMimeData>
 #include <QMouseEvent>
 #include <QPixmap>
-#if QT_VERSION < 0x050000
 #include <QUrl>
-#endif
 
 #if defined(HAVE_CONFIG_H)
 #include <config/genesis-config.h> /* for USE_QRCODE */
@@ -209,4 +208,20 @@ void ReceiveRequestDialog::on_btnCopyURI_clicked()
 void ReceiveRequestDialog::on_btnCopyAddress_clicked()
 {
     GUIUtil::setClipboard(info.address);
+}
+
+void ReceiveRequestDialog::on_btnCoinRequest_clicked()
+{
+    std::string creq = "https://coinrequest.io/create?coin=genesisnetwork&address=";
+    
+    QString cruri;
+    cruri += GUIUtil::HtmlEscape(info.address);
+    if (info.amount)
+        cruri += ("&amount=") + GenesisUnits::format(model->getDisplayUnit(), info.amount);
+    if (!info.message.isEmpty())
+        cruri += ("&message=") + GUIUtil::HtmlEscape(info.message);
+    
+    QString link = QString::fromStdString(creq + cruri.toStdString());
+
+    QDesktopServices::openUrl(QUrl(link));
 }
